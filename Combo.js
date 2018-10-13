816d44b08861c929b4fd89ab1baca31d2f2d3d9d
Hardwin Bui <buihardwin@yahoo.com>
2018-10-13 15:35:23 -0700

    Changed tabs into spaces.

--- Combo.js
+++ Combo.js
@@ -1,133 +1,133 @@
 const Card = require('./Card');
 
 class Combo{
-	
-	constructor(){
-		this.hand = [];
-	}
-	
-	// Adds a card to the current combo with order of greatest to least
-	addCard(card){
-		var isAdded = false;
-		for(var i = 0; i < this.hand.length; i++){
-			if(card.value > this.hand[i].value){
-				this.hand.splice(i, 0, card);
-				isAdded = true;
-				break;
-			}
-			else if(card.value == this.hand[i].value){
-				if(card.suit > this.hand[i].suit){
-					this.hand.splice(i, 0, card);
-					isAdded = true;
-					break;
-				}
-			} 
-		}
-		
-		if(!isAdded){	
-			this.hand.push(card);
-		}
-	}
-	
-	// Removes a card from the combo
-	removeCard(card){
-		for(var i = 0; i < this.hand.length; i++){
-			if(this.hand[i].isEquals(card)){
-				this.hand.splice(i, 1);
-				break;
-			}
-		}
-	}
+    
+    constructor(){
+        this.hand = [];
+    }
+    
+    // Adds a card to the current combo with order of greatest to least
+    addCard(card){
+        var isAdded = false;
+        for(var i = 0; i < this.hand.length; i++){
+            if(card.value > this.hand[i].value){
+                this.hand.splice(i, 0, card);
+                isAdded = true;
+                break;
+            }
+            else if(card.value == this.hand[i].value){
+                if(card.suit > this.hand[i].suit){
+                    this.hand.splice(i, 0, card);
+                    isAdded = true;
+                    break;
+                }
+            } 
+        }
+        
+        if(!isAdded){   
+            this.hand.push(card);
+        }
+    }
+    
+    // Removes a card from the combo
+    removeCard(card){
+        for(var i = 0; i < this.hand.length; i++){
+            if(this.hand[i].isEquals(card)){
+                this.hand.splice(i, 1);
+                break;
+            }
+        }
+    }
 
 
 
-	/*
-	 * Functions to determine what the type of a given combo is
-	 */
-	static isSingle(combo){
-		return combo.hand.length == 1;
-	}
+    /*
+     * Functions to determine what the type of a given combo is
+     */
+    static isSingle(combo){
+        return combo.hand.length == 1;
+    }
 
-	static isPair(combo){
-		if(combo.hand.length == 2)
-			return combo.hand[0].value == combo.hand[1].value;
-		return false;
-	}
+    static isPair(combo){
+        if(combo.hand.length == 2)
+            return combo.hand[0].value == combo.hand[1].value;
+        return false;
+    }
 
-	static isTriple(combo){
-		if(combo.hand.length == 3)
-			return combo.hand[0].value == combo.hand[1].value && combo.hand[0].value == combo.hand[2].value;
-		return false;
-	}
+    static isTriple(combo){
+        if(combo.hand.length == 3)
+            return combo.hand[0].value == combo.hand[1].value && combo.hand[0].value == combo.hand[2].value;
+        return false;
+    }
 
-	static isRun(combo){
-		// a run requires at least three cards
-		if(combo.hand.length >= 3){
-			var firstVal = combo.hand[0].value;
-			for(var i = 1; i < combo.hand.length; i++){
-				firstVal -= 1;
-				if(firstVal != combo.hand[i].value)
-					return false;
-			}
-			return true;
-		}
-		return false;
-	}
+    static isRun(combo){
+        // a run requires at least three cards
+        if(combo.hand.length >= 3){
+            var firstVal = combo.hand[0].value;
+            for(var i = 1; i < combo.hand.length; i++){
+                firstVal -= 1;
+                if(firstVal != combo.hand[i].value)
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
 
-	static isPairBomb(combo){
-		if(combo.hand.length >= 6){
-			var firstVal = combo.hand[0].value;
-			if(firstVal != combo.hand[1].value)
-				return false;
+    static isPairBomb(combo){
+        if(combo.hand.length >= 6){
+            var firstVal = combo.hand[0].value;
+            if(firstVal != combo.hand[1].value)
+                return false;
 
-			for(var i = 2; i < combo.hand.length; i += 2){
-				firstVal -= 1;
-				if(firstVal != combo.hand[i].value || firstVal != combo.hand[i+1].value)
-					return false;
-			}
-			return true;
-		}
-		return false;
-	}
+            for(var i = 2; i < combo.hand.length; i += 2){
+                firstVal -= 1;
+                if(firstVal != combo.hand[i].value || firstVal != combo.hand[i+1].value)
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
 
-	static isFourOfKind(combo){
-		if(combo.hand.length == 4)
-			return combo.hand[0].value == combo.hand[1].value &&
-					combo.hand[2].value == combo.hand[3].value &&
-					combo.hand[0].value == combo.hand[2].value;
-		return false;
-	}
+    static isFourOfKind(combo){
+        if(combo.hand.length == 4)
+            return combo.hand[0].value == combo.hand[1].value &&
+                    combo.hand[2].value == combo.hand[3].value &&
+                    combo.hand[0].value == combo.hand[2].value;
+        return false
+    }
 
-	static isValid(combo){
-		return Combo.isSingle(combo) || Combo.isPair(combo) || Combo.isTriple(combo) ||
-				Combo.isRun(combo) || Combo.isPairBomb(combo) || Combo.isFourOfKind(combo);
-	}
-	
-	// Checks if there is a 3 of spades in the combo
-	static hasThreeOfSpades(combo){
-		for(var i = 0; i < combo.hand.length; i++){
-			if(combo.hand[i].value == 0 && combo.hand[i].suit == 0)
-				return true;
-		}
-		return false;
-	}
-	
-	// Returns the amount of 2 cards in the combo
-	static getAmtOfTwos(combo){
-		var twoAmt = 0;
-		for(var i = 0; i < combo.hand.length; i++){
-			if(combo.hand[i].value == 12)
-				twoAmt += 1;
-		}
-		return twoAmt;
-	}
+    static isValid(combo){
+        return Combo.isSingle(combo) || Combo.isPair(combo) || Combo.isTriple(combo) ||
+                Combo.isRun(combo) || Combo.isPairBomb(combo) || Combo.isFourOfKind(combo);
+    }
+    
+    // Checks if there is a 3 of spades in the combo
+    static hasThreeOfSpades(combo){
+        for(var i = 0; i < combo.hand.length; i++){
+            if(combo.hand[i].value == 0 && combo.hand[i].suit == 0)
+                return true;
+        }
+        return false;
+    }
+    
+    // Returns the amount of 2 cards in the combo
+    static getAmtOfTwos(combo){
+        var twoAmt = 0;
+        for(var i = 0; i < combo.hand.length; i++){
+            if(combo.hand[i].value == 12)
+                twoAmt += 1;
+        }
+        return twoAmt;
+    }
 
-	// Prints the current combo for debugging
-	printCombo(){
-		for(var i = 0; i < this.hand.length; i++){
-			console.log(this.hand[i].value + " " + this.hand[i].suit);
-		}
-	}
+    // Prints the current combo for debugging
+    printCombo(){
+        for(var i = 0; i < this.hand.length; i++){
+            console.log(this.hand[i].value + " " + this.hand[i].suit);
+        }
+    }
 }
 
 module.exports = Combo;
--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var serv = require('http').Server(app);
 
 // Send index.html file
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/index.html');
 });
 // Send the client file that was requested
 app.use('/client', express.static(__dirname + '/client'));
@@ -32,90 +32,90 @@ c.addCard(new Card(4, 0));
 // Loading up socket.io tools
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-	// Called whenever there's a connection
-	console.log('A challenger approaches.');
-	
-	// Add player to player list
-	PLAYER_LIST.push(socket);
-	playerAmt += 1;
-//	new Game(socket, socket, socket, socket);
+    // Called whenever there's a connection
+    console.log('A challenger approaches.');
+    
+    // Add player to player list
+    PLAYER_LIST.push(socket);
+    playerAmt += 1;
+//  new Game(socket, socket, socket, socket);
 
-	for(var i = 0; i < PLAYER_LIST.length; i++)
-		console.log(PLAYER_LIST[i].id);
+    for(var i = 0; i < PLAYER_LIST.length; i++)
+        console.log(PLAYER_LIST[i].id);
 
-	// Make a game if there are four players
-	if(playerAmt >= 4){
-		game = new Game(PLAYER_LIST[0], PLAYER_LIST[1], PLAYER_LIST[2], PLAYER_LIST[3]);
-	}
+    // Make a game if there are four players
+    if(playerAmt >= 4){
+        game = new Game(PLAYER_LIST[0], PLAYER_LIST[1], PLAYER_LIST[2], PLAYER_LIST[3]);
+    }
 
 
-	// Remove player from the list if they disconnect
-	socket.on('disconnect', function(){
-		for(var i = 0; i < PLAYER_LIST.length; i++){
-			if(PLAYER_LIST[i] == socket){
-				PLAYER_LIST.splice(i, 1);
-				break;
-			}
-		}
-		playerAmt -= 1;
-		console.log('A player has left.');
-	});
-	
-	
-	
-	// Check if the combo is valid then respond accordingly
-	socket.on('newCombo', function(data){
-		// Read the inputted combo
-		var comboStr = JSON.parse(data.combo);
-		var theCombo = new Combo();
-		for(var i = 0; i < comboStr.length; i++){
-			var card = new Card(comboStr[i].value, comboStr[i].suit);
-			theCombo.addCard(card);
-		}
-		
-		// Try to play the combo and 
-		if(!game.playCombo(theCombo)){
-			// Determine which error message to send
-			var errMsg;
-			if(!Combo.isValid(theCombo))
-				errMsg = 'Your input is invalid.';
-			else if(game.curTurn <= 0 && !Combo.hasThreeOfSpades(theCombo))
-				errMsg = 'You must use the 3 of spades in your combination.';
-			else
-				errMsg = 'Your combination doesn\'t beat the current combination.';
-						
-			// Send error message to player
-			socket.emit('invalidInput', {
-				msg: errMsg,
-				curPlayer: game.curPlayer,
-			});
-		}
-		
-	});
-	
-	// Check if the current player decided to skip their turn
-	socket.on('skipRound', function(data){
-		game.passRound();
-	});
-	
+    // Remove player from the list if they disconnect
+    socket.on('disconnect', function(){
+        for(var i = 0; i < PLAYER_LIST.length; i++){
+            if(PLAYER_LIST[i] == socket){
+                PLAYER_LIST.splice(i, 1);
+                break;
+            }
+        }
+        playerAmt -= 1;
+        console.log('A player has left.');
+    });
+    
+    
+    
+    // Check if the combo is valid then respond accordingly
+    socket.on('newCombo', function(data){
+        // Read the inputted combo
+        var comboStr = JSON.parse(data.combo);
+        var theCombo = new Combo();
+        for(var i = 0; i < comboStr.length; i++){
+            var card = new Card(comboStr[i].value, comboStr[i].suit);
+            theCombo.addCard(card);
+        }
+        
+        // Try to play the combo and 
+        if(!game.playCombo(theCombo)){
+            // Determine which error message to send
+            var errMsg;
+            if(!Combo.isValid(theCombo))
+                errMsg = 'Your input is invalid.';
+            else if(game.curTurn <= 0 && !Combo.hasThreeOfSpades(theCombo))
+                errMsg = 'You must use the 3 of spades in your combination.';
+            else
+                errMsg = 'Your combination doesn\'t beat the current combination.';
+                        
+            // Send error message to player
+            socket.emit('invalidInput', {
+                msg: errMsg,
+                curPlayer: game.curPlayer,
+            });
+        }
+        
+    });
+    
+    // Check if the current player decided to skip their turn
+    socket.on('skipRound', function(data){
+        game.passRound();
+    });
+    
 });
 
 
 
 // Called every 40 mills
 setInterval(function(){
-	
-	// For each player, emit game message
-	if(playerAmt >= 4){	
-		for(var i in PLAYER_LIST){
-			var player = PLAYER_LIST[i];
-			//stuff
-			player.emit('curGame', {
-				//update the specific player with data
-				gameData: JSON.stringify(game),
-				playerNum: i,
-			});
-		}
-	}
-	
+    
+    // For each player, emit game message
+    if(playerAmt >= 4){ 
+        for(var i in PLAYER_LIST){
+            var player = PLAYER_LIST[i];
+            //stuff
+            player.emit('curGame', {
+                //update the specific player with data
+                gameData: JSON.stringify(game),
+                playerNum: i,
+            });
+        }
+    }
+    
 }, 1000/25);
